c5410341431380da3aa4864ebdd6529f67f12dc2
shashank <shashank_@outlook.in>
2025-02-26 11:00:08 +0530

    .

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,15 +16,26 @@ import merchRouter from "./routes/merchRouter";
 import orderRouter from "./routes/orderRouter";
 import dashboardRouter from "./routes/dashboardRouter";
 import cookieParser from "cookie-parser";
+import rateLimit from 'express-rate-limit';
+import mongoSanitize from 'express-mongo-sanitize';
 
 dotenv.config();
 
 const app = express();
 
+const globalLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, 
+  max: 100, 
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: 'Too many requests from this IP, please try again after 15 minutes'
+});
+
+app.use(mongoSanitize());
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "http://client:5173", "http://admin:5174","http://10.10.0.118:5173","http://10.10.0.118:5174"], // Explicitly allow frontend origins
+    origin: ["http://localhost:5173", "http://localhost:5174", "http://client:5173", "http://admin:5174","http://10.10.0.118:5173","http://10.10.0.118:5174", "apoorv.iiitkottayam.ac.in"], // Explicitly allow frontend origins
     credentials: true, // Allow cookies
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -48,12 +59,12 @@ app.use((req, res, next) => {
 // app.use("/api/v1/private", authenticateUser(),  (req, res) => {
 //   res.send("Welcome to the private route")
 // })
+app.use(globalLimiter);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/merch", merchRouter);
 app.use("/api/v1/order", orderRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
-// app.use("/api/v1/profile", profileRouter)
-// app.use("/api/v1/rides", rideRouter)
+
 
 app.use(errorHandlerMiddleware);
 app.use(notFound);
